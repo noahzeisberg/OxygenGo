e565a36bd5475629deb6fdf1f60e86e38efa427b
NoahOnFyre <105240543+NoahOnFyre@users.noreply.github.com>
2023-08-16 12:20:12 +0200

    Add OxygenTask and Oxygen structs.

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package oxygengo
 
-func main() {
+type Oxygen struct {
+  CName string
+  RName string
+  GName string
+  NWname string
+  CUsage float64
+  RUsage float64
+  GUsage float64
+  NWUsage float64
+  CSys float64
+  RSys float64
+  GSys float64
+  NWSys float64
+  CUsr float64
+  RUsr float64
+  GUsr float64
+  NWUsr float64
+  OTask OxygenTask
+  Tasks []OxygenTask{}
+}
+
+type OxygenTask struct {
+  Name string
+  PID int
+  CImpact float64
+  RImpact float64
+  GImpact float64
+  NWImpact float64
+}
+
+func init() {
   
+}
+
+func GetOxygen() {
+  return Oxygen{}
 }
